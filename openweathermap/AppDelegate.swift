354cd1756155ba463a94e978e6c2d33ac3c8e2e9
Мялин Валентин <valic.my@gmail.com>
2015-03-22 14:13:52 +0200

    Initial Commit

---
+++ openweathermap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  openweathermap
+//
+//  Created by Мялин Валентин on 3/22/15.
+//  Copyright (c) 2015 Мялин Валентин. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ openweathermap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  openweathermap
+//
+//  Created by Мялин Валентин on 3/22/15.
+//  Copyright (c) 2015 Мялин Валентин. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
